bb3f5f27463fb68aa27dda37768ddcdea26681f1
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2021-10-21 06:16:14 +0100

    feat: the AdvertCampaign model now uses the media library to upload and stoes images.

--- app/Models/AdvertCampaign.php
+++ app/Models/AdvertCampaign.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Enums\MediaCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class AdvertCampaign extends Model
+class AdvertCampaign extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
+
+    /**
+     * Registers media collections
+     *
+     * @return void
+     */
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection(MediaCollection::BANNER)
+            ->singleFile();
+    }
 }
