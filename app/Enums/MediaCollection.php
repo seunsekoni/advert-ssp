54cecc7a3af639614d6767002346940124c0dc59
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2021-10-21 06:15:02 +0100

    feat: created mediacollection enum types.

---
+++ app/Enums/MediaCollection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static BANNER()
+ */
+final class MediaCollection extends Enum
+{
+    public const BANNER = 'banner';
+}
